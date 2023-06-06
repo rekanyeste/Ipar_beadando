d654e61794e78af9156fc776338d8a09c95be037
Nyeste Reka <rekabeka37@gmail.com>
2023-06-06 19:41:18 +0200

    added description to the testing program

--- cameraTesting.py
+++ cameraTesting.py
@@ -1,19 +1,16 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0) # 0 -> a számítógép saját kamerája
-cap.set(3,640) # set Width
-cap.set(4,480) # set Height
+cap = cv2.VideoCapture(0) # paraméter: 0 -> a számítógép saját kameráját fogja érzékelni
+cap.set(3,640) # szélesség
+cap.set(4,480) # magasság
 while(True):
     ret, frame = cap.read()
-    #frame = cv2.flip(frame, -1) # Flip camera vertically
-    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
     cv2.imshow('frame', frame)
-    #cv2.imshow('gray', gray)
     
     k = cv2.waitKey(30) & 0xff
-    if k == 27: # press 'ESC' to quit
+    if k == 27: # nyomd le az 'ESC' gombot a kilépéshez
         break
 cap.release()
 cv2.destroyAllWindows()
