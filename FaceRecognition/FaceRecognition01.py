7d9196dc06a4eef265979dd0ebdbdc91b5f153d9
Nyeste Reka <rekabeka37@gmail.com>
2023-06-09 19:06:12 +0200

    Face recognition file edited and added explanation

--- FaceRecognition/FaceRecognition01.py
+++ FaceRecognition/FaceRecognition01.py
@@ -1,32 +1,29 @@
 import cv2
 import os
+
 cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video width
-cam.set(4, 480) # set video height
-face_detector = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-# For each person, enter one numeric face id
-face_id = input('\n enter user id end press <return> ==>  ')
-print("\n [INFO] Initializing face capture. Look the camera and wait ...")
-# Initialize individual sampling face count
-count = 0
+
+cam.set(3, 640)
+cam.set(4, 480)
+
+face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml') # cv2.data.haarcascades <- enélkül nem működik a program!!!! nem tudja beolvasni a scriptet
+face_id = input('\n Írd be az ID-d, nyomj Entert és várj! ==>  ')
+count = 0 # segédváltozó, ami azért kell hogy a mentett képek címei ne keveredjenek össze
 while(True):
     ret, img = cam.read()
-    img = cv2.flip(img, -1) # flip video image vertically
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_detector.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
         count += 1
-        # Save the captured image into the datasets folder
-        cv2.imwrite("FaceRecognition/dataset." + str(face_id) + '.' +  
-                    str(count) + ".jpg", gray[y:y+h,x:x+w])
+        cv2.imwrite("C:\Python\Ipar_beadando\FaceRecognition\dataset/User." + str(face_id) + '.' +  str(count) + ".jpg", gray[y:y+h,x:x+w])
+        # PONTOSAN!!!! meg kell adni a mappa helyét, ahová elmenti a program a képeket
         cv2.imshow('image', img)
-    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+
+    k = cv2.waitKey(100) & 0xff
     if k == 27:
         break
-    elif count >= 30: # Take 30 face sample and stop video
+    elif count >= 5: # ennyi képet csinál a program
          break
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program and cleanup stuff")
 cam.release()
 cv2.destroyAllWindows()
