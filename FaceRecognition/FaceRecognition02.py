24d97d55ace97b3665ead1735faf96c89dcab370
Nyeste Reka <rekabeka37@gmail.com>
2023-06-09 19:54:20 +0200

    Writed a new explanation line to the guide about the training

--- FaceRecognition/FaceRecognition02.py
+++ FaceRecognition/FaceRecognition02.py
@@ -7,12 +7,13 @@ path = 'C:\Python\Ipar_beadando\FaceRecognition\dataset' # elérési útja az el
 recognizer = cv2.face.LBPHFaceRecognizer_create() # ha azt a hibát dobja, hogy "cv2" has no attribute 'face' -> pip install opencv_contrib_python (ha le van töltve akkor először uninstall, majd install)
 detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml");
 
+# a függvény a dataset mappán belüli összes fotót szétszedi 2 tömbre: ID-k és arcok (faces)
 def getImagesAndLabels(path):
     imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
     faceSamples=[]
     ids = []
     for imagePath in imagePaths:
-        PIL_img = Image.open(imagePath).convert('L') # grayscale
+        PIL_img = Image.open(imagePath).convert('L') # szürkeárnyalatos képeket tanul meg a program!
         img_numpy = np.array(PIL_img,'uint8')
         id = int(os.path.split(imagePath)[-1].split(".")[1])
         faces = detector.detectMultiScale(img_numpy)
@@ -20,10 +21,8 @@ def getImagesAndLabels(path):
             faceSamples.append(img_numpy[y:y+h,x:x+w])
             ids.append(id)
     return faceSamples,ids
-print ("\n [INFO] Training faces. It will take a few seconds. Wait ...")
+
 faces,ids = getImagesAndLabels(path)
 recognizer.train(faces, np.array(ids))
-# Save the model into trainer/trainer.yml
-recognizer.write('C:/Python/Ipar_beadando/FaceRecognition/trainer/trainer.yml')
-# Print the numer of faces trained and end program
-print("\n [INFO] {0} faces trained. Exiting Program".format(len(np.unique(ids))))
+recognizer.write('C:/Python/Ipar_beadando/FaceRecognition/trainer/trainer.yml') # nagyon fontos a pontos path megadása minden esetben!!!!
+print("\n {0} arc megtanulva.".format(len(np.unique(ids))))
